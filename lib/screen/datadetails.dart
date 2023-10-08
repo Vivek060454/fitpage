d794636fa6d85553053aee8489dd17e928fb18f6
Vivek060454 <98967443+Vivek060454@users.noreply.github.com>
2023-10-08 13:45:43 +0530

    Update datadetails.dart
    done

--- lib/screen/datadetails.dart
+++ lib/screen/datadetails.dart
@@ -1,3 +1,6 @@
+import 'package:fit/screen/listofindicator.dart';
+import 'package:fit/screen/parameterscreen.dart';
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
@@ -10,6 +13,7 @@ class DataDetails extends StatefulWidget {
 }
 
 class _DataDetailsState extends State<DataDetails> {
+  static final regex = RegExp("(?=int)|(?<=w)");
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -52,7 +56,32 @@ class _DataDetailsState extends State<DataDetails> {
 
                             children: [
                               for(var i=0;i<widget.data.criteria.length;i++)...[
+                        RichText(
+                        text: TextSpan(
+                        children: <InlineSpan>[
+                            for (String text in widget.data.criteria[i].text.toString().split(regex))
+                        text.startsWith('%')
+                            ? TextSpan(
+                          text: text.substring(1, text.length - 1),
+                          style: const TextStyle(
+                              decoration: TextDecoration.underline, color: Colors.blue),
+                          recognizer: TapGestureRecognizer()
+                            ..onTap = () {
+                              widget.data.criteria[i].variable.forEach((key,value){
+                                if(key.type=='indicator'){
+                                  Navigator.push(context, MaterialPageRoute(builder: (context)=>Listofindicator(studytype:key.type.studyType,defaultvalue:key.type.defaultValue)));
 
+                                }
+                                if(key.type=='value'){
+                                  Navigator.push(context, MaterialPageRoute(builder: (context)=>Values(key.type.values)));
+
+                                }
+                              });
+                            },
+                        )
+                            : TextSpan(text: text),
+                      ],
+                    )),
                                 Padding(
                                   padding: const EdgeInsets.only(top: 10),
                                   child: Text(widget.data.criteria[i].text.toString(),style: GoogleFonts.alegreya(textStyle:TextStyle(color: Colors.white,fontSize: 19,fontWeight: FontWeight.w500),),),
